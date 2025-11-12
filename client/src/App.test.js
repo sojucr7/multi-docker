e1ba1dcf6a5c6bc7cc0d449edbaa08e75bdb36c1
soju thomas <sojucr7@gmail.com>
2025-11-12 21:26:36 +0530

    test file updated

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,5 +4,6 @@ import Test from './Test';
 test('renders Im some other page!', () => {
   render(<Test />);
   const linkElement = screen.getByText(/Im some other page!/i);
+  
   expect(linkElement).toBeInTheDocument();
 });;
